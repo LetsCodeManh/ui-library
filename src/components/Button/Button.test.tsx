739a50588f44b2ca30e5ebee9efa719b0bbd7477
LetsCodeManh <manh.ng@icloud.com>
2025-06-14 11:49:14 +0200

    some changes

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react"
 import { describe, expect, it } from "vitest";
 import { Button } from "./Button";
 
@@ -6,6 +6,6 @@ describe("Button", () => {
   it("renders the button with children", () => {
     render(<Button>Click me</Button>);
     const buttonElement = screen.getByText(/Click me/i);
-    expect(buttonElement).toBeDefined;
-  });
-});
+    expect(buttonElement).toBe;
+  })
+})
